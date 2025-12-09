045a912a8b5ed59ed985ae53bc8eeb0d49d4dc4c
johamc <joha-monsalve@hotmail.com>
2025-12-09 15:06:31 -0500

    tercer ejercicio

--- src/java5control/Main.java
+++ src/java5control/Main.java
@@ -2,8 +2,8 @@ package java5control;
 
 public class Main {
     public static void main(String[] args) {
-        //FacturacionSupermercado.ejercicio1();
-        //veterinario.ejercicio2();
+        FacturacionSupermercado.ejercicio1();
+        veterinario.ejercicio2();
         parqueadero.ejercicio3();
 
     }
